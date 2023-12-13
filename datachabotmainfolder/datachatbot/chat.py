74769066f8a26309802f08d5339c0af71b9f56d6
karolvsrex <152913477+karolvsrex@users.noreply.github.com>
2023-12-13 17:21:15 -0500

    Add files via upload

---
+++ datachabotmainfolder/datachatbot/chat.py
@@ -0,0 +1,77 @@
+import streamlit as st
+
+from components.qa_model import QA_Model
+from components.documents import Documents
+from chat_view_model import ChatViewModel
+
+def create_application(view_model: ChatViewModel):
+    if view_model.show_chat():
+        st.session_state.show_chat = True
+
+    if "show_chat" not in st.session_state:
+        create_setup(view_model = view_model)
+    else:
+        create_chat(view_model = view_model)
+        
+def create_setup(view_model: ChatViewModel):
+    st.title("Data Chatbot Setup")
+    st.info("No database was found. Please add any files you want to constitute the database. Once done, click on Process")
+    
+    uploaded_files = st.file_uploader("Please upload your PDF files", type=["pdf", "docx"], accept_multiple_files=True)
+    
+    if st.button("Process", type="primary"):        
+        with st.spinner('Moving your files into docs folder'):
+            for uploaded_file in uploaded_files:
+                # Check file type and process accordingly
+                if uploaded_file.type == "application/pdf":
+                    # Extract text from PDF
+                    text = extract_text_from_pdf(uploaded_file)
+                    # TODO: Process or save the extracted text
+                elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                    # TODO: Handle DOCX files (if needed)
+                    pass
+                else:
+                    # TODO: Handle other file types or show an error message
+                    pass
+
+        with st.spinner('Ingesting documents...'):
+            view_model.ingest_docs()
+            st.session_state.show_chat = True
+            st.rerun()
+
+
+def create_chat(view_model: ChatViewModel):
+    st.title("Data Chatbot")
+
+    with st.sidebar:
+        st.info('Press this button if you want to clear the database', icon="ℹ️")
+        if st.button("Clear database", type="primary"):
+            view_model.clear_database()
+            del st.session_state.show_chat
+            st.rerun()
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What's your question?"):
+
+        # Add user message to the chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        
+        # Display user message
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Display assistant response
+        with st.chat_message("assistant"):
+            message_placeholder = st.markdown("AI is thinking...")
+            response = view_model.generate_response(prompt)
+            message_placeholder.markdown(response)
+            st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ datachabotmainfolder/datachatbot/components/document_splitter.py
@@ -0,0 +1,28 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 125
+
+class DocumentSplitter:
+    def create_chunks(self):
+        print ("Loading documents...")
+        loader = DirectoryLoader(
+            path='./docs',
+            glob="**/*",
+            show_progress=False,
+            use_multithreading=True
+        )
+        print ("Documents loaded")
+        print ("Creating chunks...")
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = CHUNK_SIZE,
+            chunk_overlap = CHUNK_OVERLAP,
+            length_function = len,
+        )
+
+        chunks = loader.load_and_split(
+            text_splitter=text_splitter
+        )
+        print("Created", len(chunks), "chunks of data")
+        return chunks
